49b491984db12fd9b48552e96ccc80d2346baf37
kseniya7991 <kseniya7991@gmail.com>
2024-04-02 21:47:05 +0300

    add scss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import glsl from 'vite-plugin-glsl'
+import glsl from "vite-plugin-glsl";
+import sassDts from "vite-plugin-sass-dts";
 
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
+    server: {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
+    build: {
+        outDir: "../dist", // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
     },
-    plugins:
-    [
-        glsl()
-    ]
-}
+    plugins: [glsl(), sassDts()],
+};
